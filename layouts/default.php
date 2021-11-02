e8401ee9bae3179df57255bf31fb7774e100dffc
Egze669 <macwal1234@gmail.com>
2021-11-02 20:11:06 +0100

    Automatyzowany URL

--- layouts/default.php
+++ layouts/default.php
@@ -2,11 +2,11 @@
 <head>
 	<title><?php echo $title ?></title>
 </head>
-<body>
+<body>?>
 	<h1>Hello APSL!</h1>
-    <a href='/'>Main PAGE</a><br />
-    <a href='/body'>BODY PAGE</a><br />
-    <a href='/article'>Article PAGE</a>
+    <a href=<?php $url ?>>Main PAGE</a><br />
+    <a href=<?php $url ?>>BODY PAGE</a><br />
+    <a href=<?php $url ?>>Article PAGE</a>
 <!--    napisac generate uri, ma zadanie przyjac 2 parametry.-->
 <!--    1 nazwa, 2 parametry routingu w wyniku-->
 <!--    /strona/parametry w oparciu o router match-->
